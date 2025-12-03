6f99a1c5d818749c5abf54a892bd00ff88dba2a2
Sadasree587 <sadasadasree@gmail.com>
2025-12-03 13:10:36 +0530

    Add menu item click highlighting functionality

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Highlight clicked menu item
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach(item => {
+    item.addEventListener("click", () => {
+        navItems.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    });
+});
